15ee1cc3803c18aaee06e88d0b4ca51a4560214a
ProjectSkeleton <116695918+ProjectSkeleton@users.noreply.github.com>
2022-10-30 02:15:21 +0000

    Switch to glm::value_ptr

--- src/delta/delta/shader/uniform_buffer.cc
+++ src/delta/delta/shader/uniform_buffer.cc
@@ -1,25 +1,27 @@
 #include "uniform_buffer.hh"
 
+#include <glm/gtc/type_ptr.hpp>
+
 namespace Delta {
 
 UniformBuffer::UniformBuffer(const UniformBufferInfo& uniform_info) : info_(uniform_info) { }
 
 void UniformBuffer::Upload(float f) { Upload(&f); }
-void UniformBuffer::Upload(const glm::vec2& vec) { Upload(&vec); }
-void UniformBuffer::Upload(const glm::vec3& vec) { Upload(&vec); }
-void UniformBuffer::Upload(const glm::vec4& vec) { Upload(&vec); }
-void UniformBuffer::Upload(const glm::mat4& matrix) { Upload(&matrix); }
+void UniformBuffer::Upload(const glm::vec2& vec) { Upload(glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const glm::vec3& vec) { Upload(glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const glm::vec4& vec) { Upload(glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const glm::mat4& matrix) { Upload(glm::value_ptr(matrix)); }
 
 void UniformBuffer::Upload(const std::string& member_name, float f) { Upload(member_name, &f); }
-void UniformBuffer::Upload(const std::string& member_name, const glm::vec2& vec) { Upload(member_name, &vec); }
-void UniformBuffer::Upload(const std::string& member_name, const glm::vec3& vec) { Upload(member_name, &vec); }
-void UniformBuffer::Upload(const std::string& member_name, const glm::vec4& vec) { Upload(member_name, &vec); }
-void UniformBuffer::Upload(const std::string& member_name, const glm::mat4& matrix) { Upload(member_name, &matrix); }
+void UniformBuffer::Upload(const std::string& member_name, const glm::vec2& vec) { Upload(member_name, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const std::string& member_name, const glm::vec3& vec) { Upload(member_name, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const std::string& member_name, const glm::vec4& vec) { Upload(member_name, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(const std::string& member_name, const glm::mat4& matrix) { Upload(member_name, glm::value_ptr(matrix)); }
 
 void UniformBuffer::Upload(size_t member_index, float f) { Upload(member_index, &f); }
-void UniformBuffer::Upload(size_t member_index, const glm::vec2& vec) { Upload(member_index, &vec); }
-void UniformBuffer::Upload(size_t member_index, const glm::vec3& vec) { Upload(member_index, &vec); }
-void UniformBuffer::Upload(size_t member_index, const glm::vec4& vec) { Upload(member_index, &vec); }
-void UniformBuffer::Upload(size_t member_index, const glm::mat4& matrix) { Upload(member_index, &matrix); }
+void UniformBuffer::Upload(size_t member_index, const glm::vec2& vec) { Upload(member_index, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(size_t member_index, const glm::vec3& vec) { Upload(member_index, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(size_t member_index, const glm::vec4& vec) { Upload(member_index, glm::value_ptr(vec)); }
+void UniformBuffer::Upload(size_t member_index, const glm::mat4& matrix) { Upload(member_index, glm::value_ptr(matrix)); }
 
 }
--- src/delta/delta/shader/uniform_buffer.hh
+++ src/delta/delta/shader/uniform_buffer.hh
@@ -6,7 +6,6 @@
 #include <unordered_map>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 #include "delta/delta/model/buffer_layout.hh"
 #include "delta/delta/shader/shader_stage.hh"
